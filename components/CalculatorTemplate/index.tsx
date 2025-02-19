52332606fc112c50d37297eb300059db15720172
Vishwajeet Kondi <vkondi@gmail.com>
2025-02-19 11:54:34 +0530

    chore: test husky pre commit

--- components/CalculatorTemplate/index.tsx
+++ components/CalculatorTemplate/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 // CalculatorTemplate.jsx
 
-import React, { FC, PropsWithChildren, useCallback, useMemo } from "react";
+import React, { FC, PropsWithChildren, useCallback } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
